a5ba7f546f63fb4458df408578211014244057f2
Tomi Sueldo <tomas.sueldo@lightit.io>
2025-01-17 15:32:37 -0300

    fix: corrections applied

--- src/Exceptions/InvalidGoogleTokenException.php
+++ src/Exceptions/InvalidGoogleTokenException.php
@@ -5,12 +5,12 @@ declare(strict_types=1);
 namespace Lightit\Exceptions;
 
 use Flugg\Responder\Exceptions\Http\HttpException;
-use Symfony\Component\HttpFoundation\Response;
-use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
 
 class InvalidGoogleTokenException extends HttpException
 {
-    protected $status = Response::HTTP_BAD_REQUEST;
-    protected $errorCode = 'Invalid_google_token_exception';
+    protected $status = JsonResponse::HTTP_BAD_REQUEST;
+
+    protected $errorCode = 'Invalid_google_token';
 
 }
--- src/Exceptions/UnauthorizedException.php
+++ src/Exceptions/UnauthorizedException.php
@@ -6,12 +6,11 @@ namespace Lightit\Exceptions;
 
 use Flugg\Responder\Exceptions\Http\HttpException;
 use Illuminate\Http\JsonResponse;
-use Symfony\Component\HttpFoundation\Response;
-use Illuminate\Http\Request;
 
 class UnauthorizedException extends HttpException
 {
     protected $status = JsonResponse::HTTP_UNAUTHORIZED;
+    
     protected $errorCode = 'unauthorized';
 
 }
