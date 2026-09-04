d585f1442fc481e427e7e92b81a6b1bce58ef672
Tomi Sueldo <tomas.sueldo@lightit.io>
2026-09-04 18:06:46 +0200

    feat(auth): add Feature::Passkeys enum case

--- src/Enums/Feature.php
+++ src/Enums/Feature.php
@@ -11,6 +11,7 @@ enum Feature: string
     case Otp = 'otp';
     case ForgotPassword = 'forgot-password';
     case GoogleSso = 'google-sso';
+    case Passkeys = 'passkeys';
 
     public function label(): string
     {
@@ -18,8 +19,28 @@ enum Feature: string
             self::TwoFactorAuthentication => 'Two-Factor Authentication',
             self::RolesAndPermissions => 'Roles and Permissions',
             self::Otp => 'OTP (one-time password)',
-            self::ForgotPassword => 'Forgot Password',
+            self::ForgotPassword => 'Forgot Password flow',
             self::GoogleSso => 'Google SSO',
+            self::Passkeys => 'Passkeys (WebAuthn)',
         };
+    }
+
+    /**
+     * @return list<string>
+     */
+    public static function slugs(): array
+    {
+        return array_column(self::cases(), 'value');
+    }
+
+    /**
+     * @return array<string, string>
+     */
+    public static function options(): array
+    {
+        return array_combine(self::slugs(), array_map(
+            fn (self $f) => $f->label(),
+            self::cases()
+        ));
     }
 }
